95bbd1b324eba439cdba580c821882017293d2af
Jessrey Vincent Garrido <128167254+jessrey619@users.noreply.github.com>
2024-04-01 22:10:21 +0800

    For the Front End Folder
    This contains the initialization for the project folder. Not Yet Edited. Sort of. Nothing significant

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      {/* THIS SHOULD BE THE LANDING PAGE BUT IF NAAY MU HIMO CHILL */}
+    </div>
+  );
+}
+
+export default App;

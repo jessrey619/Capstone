fa6ade70cfe03b092d935dbca5d0c4f31c82f2ac
Phoebe Kae <103922664+Pbkae@users.noreply.github.com>
2024-04-08 20:06:15 +0800

    index.js
    rewrote index.js and removed the header and sidebar in the app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Swtih, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'remixicon/fonts/remixicon.css'
 
@@ -13,13 +13,11 @@ import SideBar from './Components/SideBar/SideBar';
 
 function App() {
   return (
-    <>
-    <Router>
-      <Header/>
-      <SideBar/>
-    </Router>
-    </>
+    <div className='app'>
+      {/* <Routes>
+      </Routes> */}
+    </div>
   );
-}
+};
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path='/' element={<App/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 reportWebVitals();

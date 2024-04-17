75f6f9e59e1ed6b52f4043023ee5f59690eb708b
Jessrey Vincent Garrido <128167254+jessrey619@users.noreply.github.com>
2024-04-17 10:29:50 +0800

    Backend Updates and Additions
    Updates for OTP API and Addition of User and Image APIs (Incomplete)

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route>
-          <Route path='/' element={<App/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+      <App/>
+    </BrowserRouter>  
+  </React.StrictMode>
+);
+
+
 reportWebVitals();

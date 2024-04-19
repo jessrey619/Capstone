daba94df95233223aa6f4253ab526bde79756266
Jessrey Vincent Garrido <128167254+jessrey619@users.noreply.github.com>
2024-04-19 22:08:35 +0800

    Final Commitment before Arrangement

--- my-app/src/pages/EmailVerification.jsx
+++ my-app/src/pages/EmailVerification.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { EmailVerificationModal } from "../components/ModalEmailVerification/EmailVerificationModal";
-import "../css/EmailVerification.css"
+import "../CSS/EmailVerification.css"
 
 function EmailVerification() {
 

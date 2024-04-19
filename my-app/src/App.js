9b6a49a6fb29d8764b0fe4676848401509f32204
TracyMartinez810 <martineztracy810@gmail.com>
2024-04-20 01:18:56 +0800

    merging

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
 
 
 // import logo from './logo.svg';
 // import Axios from 'axios'
-
-import './App.css';
-import Login from './components/Login/Login';
-import ChangePassword from './components/Login/ChangePassword';
-import LoginBgm from './components/Login/LoginBgm';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import 'bootstrap-icons/font/bootstrap-icons.css'
-// import 'remixicon/fonts/remixicon.css'
-=======
 import Axios from 'axios'
 
 import './App.css';
 import { Route, Routes} from 'react-router-dom';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'remixicon/fonts/remixicon.css'
->>>>>>> origin/main
 
 // import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/js/bootstrap.min.js'
@@ -26,8 +15,7 @@ import 'remixicon/fonts/remixicon.css'
 // import Header from './Components/Header/Header';
 // import SideBar from './Components/SideBar/SideBar';
 
-<<<<<<< HEAD
-=======
+
 // Phoebe
 import Header from './Components/Header/Header';
 import SideBar from './Components/SideBar/SideBar';
@@ -42,7 +30,6 @@ import Configuration from './pages/admin/Configuration';
 import Homepage from './pages/homepage';
 import Homepage_Employee from './pages/homepage_employee';
 import Submit from './pages/submit';
->>>>>>> origin/main
 
 // Jessrey
 // import EmailVerification from './pages/EmailVerification';
@@ -57,7 +44,6 @@ import TheHeader from './Components/Header/UserHeader';
 
 function App() {
   return (
-<<<<<<< HEAD
 
     <div className='app'>
       {/* <Routes>
@@ -65,21 +51,7 @@ function App() {
 
       <LoginBgm/>
     </div>
-=======
-    <>
-      <Header/>
-      <SideBar/>
-      <Routes>
-        {/* Admin */}
-        <Route path='/' element={<Dashboard/>} />
-        <Route path='/application-list' element={<ApplicationList/>} />
-        <Route path='/logs' element={<Logs/>} />
-        <Route path='/statistics' element={<Statistics/>} />
-        <Route path='/parking-area' element={<ParkingArea/>} />
-        <Route path='/configuration' element={<Configuration/>} />
-      </Routes>
-    </>
->>>>>>> origin/main
+   
   );
   
 };

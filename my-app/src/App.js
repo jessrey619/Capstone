c6becd890e52ec091210f7a5efccac9039bd0933
Phoebe Kae <103922664+Pbkae@users.noreply.github.com>
2024-04-19 22:32:31 +0800

    Merge PhoebeBranch into main

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,65 @@
-
-
-//import logo from './logo.svg';
-import Axios from 'axios'
-
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'remixicon/fonts/remixicon.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+// Phoebe
+import Header from './Components/Header/Header';
+import SideBar from './Components/SideBar/SideBar';
+import Dashboard from './pages/Dashboard'
+import ApplicationList from './pages/ApplicationList'
+import Logs from './pages/Logs'
+import Statistics from './pages/Statistics'
+import ParkingArea from './pages/ParkingArea'
+ 
+// Therese
 import Homepage from './pages/homepage';
 import Homepage_Employee from './pages/homepage_employee';
 import Submit from './pages/submit';
 
-
+// Jessrey
 import EmailVerification from './pages/EmailVerification';
 import ViewImages from './components/InsertImageTest/ViewImages';
 import AddImage from './components/InsertImageTest/AddImage';
 import PhotoGallery from './components/InsertImageTest/PhotoGallery';
-import ChangePassword from './pages/ChangePassword';
-import TheHeader from './components/Header';
 
+//Tracy
+import ChangePassword from './pages/ChangePassword';
+
+// Reina
+import TheHeader from './components/Header';
 
 
 function App() {
   return (
-
-    <div className='app'>
-      <TheHeader/>
+  <>
+  <Header/>
+  <SideBar/>
       <Routes>
-        <Route index element={<EmailVerification/>} path='/Register/EmailVerification'/>
-        <Route index element={<ViewImages/>} path='/Images/'/>
-        <Route index element={<AddImage/>} path='/Images/add'/>
-        <Route index element={<PhotoGallery/>} path='/photos/'/>
-        <Route index element={<ChangePassword/>} path='/Account/ChangePassword' />
-        <Route index element={<Homepage_Employee/>} path='/Home/Employee' />
-          
+        {/* User */}
+          <Route path='/' element={<Dashboard/>} />  {/* E change ni into landing page na route placeholder ra ning dashboard */}
+          {/* Tanan page sa user e butang diri 
+          */}
+
+        
+        {/* Admin */}
+          <Route path='/' element={<Dashboard/>} />
+          <Route path='/application-list' element={<ApplicationList/>} />
+          <Route path='/logs' element={<Logs/>} />
+          <Route path='/statistics' element={<Statistics/>} />
+          <Route path='/parking-area' element={<ParkingArea/>} />
+          <Route path='/configuration' element={<Configuration/>} />
+          <Route index element={<ChangePassword/>} path='/Account/ChangePassword' />
+          <Route index element={<Homepage_Employee/>} path='/Home/Employee' />
       </Routes>
-    </div>
+  </>
   );
+
+  
 };
 
 export default App;
+
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-import TheHeader from './components/Header';
-import Login from './pages/Login';
-import ChangePassword from './pages/ChangePassword';
-import Registration from './pages/Registration';
-import LoginTrial from './pages/LoginTrial';
-
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
-
+  </React.StrictMode>
+);
 
 reportWebVitals();
